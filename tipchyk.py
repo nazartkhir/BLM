92ecd33bc50bafa03811e974ab93550991a26e48
Nazar Tkhir <nazar.tchir@gmail.com>
2023-05-27 00:56:55 +0300

    food gen cluster

--- tipchyk.py
+++ tipchyk.py
@@ -21,69 +21,58 @@ class Races:
     ORANGE = (255, 165, 0)
     GREEN = (0, 255, 0)
 
+
+
+
 class World:
     def __init__(self, size, people) -> None:
         self.food_count = 0
         self.max_food_count = 0
+        self.clusters = []
         self.grid = [[0 for _ in range(size)] for _ in range(size)]
         self.size = size
         for i, race in enumerate(people):
             for person in race:
                 self.grid[person.cords[0]][person.cords[1]] = person
-        self.generate_havka(10)
+        self.generate_havka(self.size//20)
+        
         self.recharge()
 
-    def generate_havka(self, radius,):
-        def _create_cluster_spawn(grid, center_coords, radius,prob_decrease):
-            '''
-            Takes the center of cluster and radius.
-            Further from center means less probability of spawning food.
-            '''
-
-            for loc_rad in range(1,radius):
-                for i in range(center_coords[1]-loc_rad,center_coords[1]+loc_rad):
-                    for j in range(center_coords[0]-loc_rad,center_coords[0]+loc_rad):
-                        try:
-                            if random.random() < 1/loc_rad-prob_decrease and grid[i][j] == 0:
-                                grid[i][j] = Havka()
-                                self.food_count += 1
-                        except IndexError:
-                            continue
-
-        for tup in [(radius//2, radius//2),
-                    (self.size-radius//2-1, radius//2-1),
-                    (radius//2-1, self.size-radius//2-1),
-                    (self.size-radius//2-1, self.size-radius//2-1)]:
-            _create_cluster_spawn(self.grid, tup, radius, 0.05)
-        # [_create_cluster_spawn(self.grid, (coord1//2, coord2//2),random.randint(5,10), random.random()/10) for coord1, coord2 in zip([random.choice([i for i in range(10)]) for _ in range(10)], [random.choice([i for i in range(10)]) for _ in range(10)])]
+    def generate_havka(self, radius):
+        for tup in [(radius, radius),
+                    (self.size-radius-1, radius),
+                    (radius, self.size-radius-1),
+                    (self.size-radius-1, self.size-radius-1)]:
+            cluster = Cluster(self.grid, tup, radius)
+            self.clusters.append(cluster)
+        big_ass_cluster = Cluster(self.grid,(self.size//2, self.size//2), self.size//15)
+        self.clusters.append(big_ass_cluster)
         rand_y = []
         for _ in range(3):
-            rand_y.append(random.choice(list(range(-10,5))))
+            rand_y.append(random.choice(list(range(-10,-5))))
         for _ in range(3):
             rand_y.append(random.choice(list(range(-5,0))))
         for _ in range(3):
             rand_y.append(random.choice(list(range(0,5))))
         for _ in range(3):
             rand_y.append(random.choice(list(range(5,10))))
-        print(rand_y)
         rand_x = []
         for _ in range(3):
-            rand_x.append(random.choice(list(range(-10,5))))
+            rand_x.append(random.choice(list(range(-10,-5))))
         for _ in range(3):
             rand_x.append(random.choice(list(range(-5,0))))
         for _ in range(3):
             rand_x.append(random.choice(list(range(0,5))))
         for _ in range(3):
             rand_x.append(random.choice(list(range(5,10))))
-        print(rand_x)
         for coord_y, coord_x in zip(rand_y, rand_x):
-            _create_cluster_spawn(self.grid, ((self.size//10)*coord_y, (self.size//10)*coord_x//2),random.randint(5,10), random.random()/10)
-
-        _create_cluster_spawn(self.grid, (self.size//2, self.size//2), 15, 0.01)
+            cluster = Cluster(self.grid,((self.size//10)*coord_y, (self.size//10)*coord_x//2),random.randint(self.size//40,self.size//25))
+            self.clusters.append(cluster)
+        
 
     def recharge(self):
         if self.food_count < (self.size ** 2)//8:
-            for _ in range((self.size**2)//10000):
+            for _ in range((self.size**2)//50):
                 x = random.randint(0,self.size - 1)
                 y = random.randint(0,self.size - 1)
                 if self.grid[y][x] == 0:
@@ -94,11 +83,56 @@ class World:
             for j in range(self.size):
                 if isinstance(self.grid[i][j], Tip4yk):
                     self.grid[i][j].run(self)
-        self.recharge()
+        for cluster in self.clusters:
+            cluster.refill(self.grid)
+        print(len(self.clusters))
 
     def __str__(self):
         return str('\n'.join(str(line) for line in self.grid))
-    
+
+
+class Cluster():
+    def __init__(self,grid, cords, radius) -> None:
+        self.cords = cords
+        self.radius = radius
+        self.cur = 0
+        self.limit = int((2*radius+1)**2)
+        self.spawn(grid)
+        
+
+    def spawn(self, grid):
+        for loc_rad in range(1,self.radius+1):
+            for i in range(self.cords[0]-loc_rad,self.cords[0]+loc_rad+1):
+                for j in range(self.cords[1]-loc_rad,self.cords[1]+loc_rad+1):
+                    try:
+                        if random.random() < 1/loc_rad-0.03 and grid[i][j] == 0:
+                            grid[i][j] = Havka()
+                            self.cur += 1
+                    except IndexError:
+                        continue
+    def refill(self, grid):
+        self.cur = 0
+        for loc_rad in range(1,self.radius+1):
+            for i in range(self.cords[0]-loc_rad,self.cords[0]+loc_rad+1):
+                for j in range(self.cords[1]-loc_rad,self.cords[1]+loc_rad+1):
+                    try:
+                        if isinstance(grid[i][j], Havka):
+                            self.cur += 1
+                    except IndexError:
+                        continue
+        counter = self.limit - self.cur 
+        for _ in range(1000):
+            if counter > 0:
+                y = random.randint(self.cords[0]-self.radius, self.cords[0]+self.radius)
+                x = random.randint(self.cords[1]-self.radius, self.cords[1]+self.radius)
+                try:
+                    if grid[y][x] == 0:
+                        grid[y][x] = Havka()
+                        counter -= 1
+                except IndexError:
+                    continue
+            
+
 
 class Havka():
     def __init__(self) -> None:
@@ -130,7 +164,8 @@ class Tip4yk:
             return None
         return random.choice(ans)
     def find_nearby_enemy(self, grid):
-        for r in range(1,5):
+        ans = []
+        for r in range(1,6):
             for i in range(-r,r+1):
                 for j in range(-r,r+1):
                     if i == 0 and j == 0:
@@ -139,20 +174,29 @@ class Tip4yk:
                         if isinstance(grid[self.cords[0]+i][self.cords[1]+j], Tip4yk)\
                             and self.cords[0]+i>= 0 and self.cords[1]+j>=0:
                             if grid[self.cords[0]+i][self.cords[1]+j].race != self.race:
-                                return (self.cords[0]+i, self.cords[1]+j)
+                                ans.append((self.cords[0]+i, self.cords[1]+j))
                     except IndexError:
                         continue
-        return None
+        if not ans:
+            return None
+        closest_enemy = ans[0]
+        for enemy in ans:
+            if math.dist(self.cords, enemy) < math.dist(self.cords, closest_enemy):
+                closest_enemy = enemy
+        return closest_enemy
+        # return random.choice(ans)
 
     def move(self, world, new_cords):
-        world.grid[self.cords[0]][self.cords[1]] = 0
-        self.cords[0] = new_cords[0]
-        self.cords[1] = new_cords[1]
-        if isinstance(world.grid[new_cords[0]][new_cords[1]], Havka):
-            self.energy += world.grid[new_cords[0]][new_cords[1]].recharge
-            world.food_count -= 1
-        world.grid[new_cords[0]][new_cords[1]] = self
-        self.energy -= 1
+        if not isinstance(world.grid[new_cords[0]][new_cords[1]], Tip4yk):
+            world.grid[self.cords[0]][self.cords[1]] = 0
+            self.cords[0] = new_cords[0]
+            self.cords[1] = new_cords[1]
+            if isinstance(world.grid[new_cords[0]][new_cords[1]], Havka):
+                self.energy += world.grid[new_cords[0]][new_cords[1]].recharge
+                world.food_count -= 1
+            world.grid[new_cords[0]][new_cords[1]] = self
+            self.energy -= 1
+
 
     def random_move(self, world):
         grid = world.grid
@@ -197,10 +241,32 @@ class Tip4yk:
         return self.cords
 
     def decide_state(self, world):
+        nearby_enemy = self.find_nearby_enemy(world.grid)
         if self.energy < 0:
             self.state = States.dead
-        if self.state == States.hungry and self.energy >80:
-            self.state = States.rage
+            return
+        if self.state == States.hungry:
+            if self.energy > 50:
+                if nearby_enemy:
+                    self.state = States.rage
+                else:
+                    self.state = States.exploring
+            else:
+                self.state == States.hungry
+        if self.state == States.exploring:
+            if self.energy <= 50:
+                self.state = States.hungry
+            elif nearby_enemy:
+                self.state = States.rage
+            else:
+                self.state = States.exploring
+        if self.state == States.rage:
+            if self.energy <= 50:
+                self.state = States.hungry
+            elif nearby_enemy:
+                self.state = States.rage
+            else:
+                self.state = States.exploring
 
     def decide_action(self, world):
         if self.state == States.dead:
@@ -221,22 +287,21 @@ class Tip4yk:
                 self.move(world,(self.cords[0] + diffy, self.cords[1] + diffx))
             else:
                 self.random_move(world)
-
-
         if self.state == States.rage:
             grid = world.grid
             nearby_enemy = self.find_nearby_enemy(grid)
             if nearby_enemy:
                 if int(math.dist(nearby_enemy, self.cords)) == 1:
                     loser = self.fight(nearby_enemy)
-                    print(f'Hope negro was killed')
+                    grid[loser[0]][loser[1]].state = States.dead
                     grid[loser[0]][loser[1]] = 0
                 else:
                     move = self.short_distance(grid, nearby_enemy)
                     self.move(world, move)
             else:
-                self.state = States.hungry
-                move = self.random_move(world)
+                self.random_move(world)
+        if self.state == States.exploring:
+            self.random_move(world)
 
 
     def run(self, world):
@@ -262,9 +327,6 @@ def main(size):
         visualize(world, CELL_SIZE, GAP_SIZE, WINDOW_SIZE, window)
         world.run_day()
     pygame.quit()
-
-
-
 
 
 def visualize(world, CELL_SIZE, GAP_SIZE, WINDOW_SIZE, window):
